108fa39ff6329c627b7f9db13bf3c1c7b6a28957
Juan <juhernandezm@unal.edu.co>
2023-05-31 20:07:56 -0500

    pendiente corregir addons list

--- src/Step3/index.jsx
+++ src/Step3/index.jsx
@@ -3,13 +3,17 @@ import './Step3.css'
 
 function Step3(){
     const [addons, setAddons] = React.useState([])
-
+    let idsChecked = []
+    console.log(addons);
+    addons.forEach((entry)=>{idsChecked.push(entry.id)})
     function selectAddons(event){
         if(event.target.classList.contains('inputs')){
+            const idAddon=event.target.id
             const rawPrice=event.target.nextElementSibling.lastChild.innerHTML.split('')[2]
             const titleAddon=event.target.nextElementSibling.childNodes[2].firstChild.innerHTML
             const newEntry = {
                 title : titleAddon,
+                id : idAddon,
                 price : rawPrice
             }
             if(addons.length===0){
@@ -22,11 +26,15 @@ function Step3(){
                         const newArray = [...addons]
                         const index = newArray.indexOf(newEntry)
                         newArray.splice(index, 1)
+                        console.log('erased');
                         setAddons(newArray)
                     }else{
-                        const newArray = [...addons]
-                        newArray.push(newEntry)
-                        setAddons(newArray)
+                        if(addons.length<=3){
+                            const newArray = [...addons]
+                            newArray.push(newEntry)
+                            console.log('pushed');
+                            setAddons(newArray)
+                        }
                     }
                 })  
             }     
@@ -42,8 +50,8 @@ function Step3(){
                     <div className='pickInputBox'>
                         <input type="checkbox" id='online' name='online' className='inputs pickInputs'/>
                         <label className="pickLabels labels" htmlFor="online">
-                            <span className='fa-regular fa-square fa-xl icons '></span>
-                            <span className='fa-solid fa-square-check fa-xl check inactive'></span>
+                            <span className={`fa-regular fa-square fa-xl ${!idsChecked.includes('online')?'icons':'inactive'}`}></span>
+                            <span className={`fa-solid fa-square-check fa-xl  ${idsChecked.includes('online')?'check':'inactive'}`}></span>
                             <div className="textWrap pickLabels--textWrap">
                                 <p className="pickTitle inputTitle">Online service</p>
                                 <p className="pickDescription inputDescription">Access to multiplayer games</p>
@@ -55,8 +63,8 @@ function Step3(){
                     <div className='pickInputBox'>
                         <input type="checkbox" id='storage' name='storage' className='inputs pickInputs'/>
                         <label className="pickLabels labels" htmlFor="storage">
-                            <span className='fa-regular fa-square fa-xl icons '></span>
-                            <span className='fa-solid fa-square-check fa-xl check inactive'></span>
+                            <span className={`fa-regular fa-square fa-xl ${!idsChecked.includes('storage')?'icons':'inactive'}`}></span>
+                            <span className={`fa-solid fa-square-check fa-xl  ${idsChecked.includes('storage')?'check':'inactive'}`}></span>
                             <div className="textWrap pickLabels--textWrap">
                                 <p className="pickTitle inputTitle">Larger storage</p>
                                 <p className="pickDescription inputDescription">Extra 1TB of cloud save</p>
@@ -69,8 +77,8 @@ function Step3(){
                     <div className='pickInputBox'>
                         <input type="checkbox" id='profile' name='profile' className='inputs pickInputs'/>
                         <label className="pickLabels labels" htmlFor="profile">
-                            <span className='fa-regular fa-square fa-xl icons '></span>
-                            <span className='fa-solid fa-square-check fa-xl check inactive'></span>
+                            <span className={`fa-regular fa-square fa-xl ${!idsChecked.includes('profile')?'icons':'inactive'}`}></span>
+                            <span className={`fa-solid fa-square-check fa-xl  ${idsChecked.includes('profile')?'check':'inactive'}`}></span>
                             <div className="textWrap pickLabels--textWrap">
                                 <p className="pickTitle inputTitle">Customizable profile</p>
                                 <p className="pickDescription inputDescription">Custom theme on your profile</p>
